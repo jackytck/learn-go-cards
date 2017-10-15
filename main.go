1fc323276367e4d0f439fcbd200c297268f3504f
Jacky Tang <jackytck@gmail.com>
2017-10-15 17:10:50 +0800

    :tada: initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	cards := newDeck()
+
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// remainingCards.print()
+
+	cards.saveToFile("my_cards")
+
+	fromDisk := newDeckFromFile("my_cards")
+	fromDisk.print()
+}
